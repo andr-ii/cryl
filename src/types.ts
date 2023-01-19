39222070cc516d162ac7218ca4580006960ff1e2
andr-ii <andr.lyt.dev@gmail.com>
2023-01-19 14:26:05 +0100

    fix: format

--- src/types.ts
+++ src/types.ts
@@ -5,5 +5,5 @@
 
 import { EncryptionType, Flags, FlagValues } from './utils/constants';
 
-export type Options = Partial<Record<typeof FlagValues[Flags], string>>;
+export type Options = Partial<Record<(typeof FlagValues)[Flags], string>>;
 export type MainOptions = { type: EncryptionType; values: Options } | undefined;
